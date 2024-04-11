a64c3890f21084dd15f123780626a2a99656d9a8
Karrics <adelk4rimov@yandex.ru>
2024-04-11 16:19:58 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace PassGeneration
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Exit_Button = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.checkNonLetter = new System.Windows.Forms.CheckBox();
+            this.checSimilar = new System.Windows.Forms.CheckBox();
+            this.passBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Exit_Button
+            // 
+            this.Exit_Button.AutoSize = true;
+            this.Exit_Button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Exit_Button.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Exit_Button.Location = new System.Drawing.Point(828, 9);
+            this.Exit_Button.Name = "Exit_Button";
+            this.Exit_Button.Size = new System.Drawing.Size(23, 22);
+            this.Exit_Button.TabIndex = 0;
+            this.Exit_Button.Text = "X";
+            this.Exit_Button.Click += new System.EventHandler(this.Exit_Button_Click);
+            this.Exit_Button.MouseLeave += new System.EventHandler(this.Exit_Button_MouseLeave);
+            this.Exit_Button.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Exit_Button_MouseMove);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(181)))), ((int)(((byte)(122)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.textBox1.Location = new System.Drawing.Point(277, 55);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(263, 42);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Генератор пароля";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.textBox1_MouseDown);
+            this.textBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.textBox1_MouseMove);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(181)))), ((int)(((byte)(179)))));
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.checkNonLetter);
+            this.panel1.Controls.Add(this.checSimilar);
+            this.panel1.Controls.Add(this.passBox);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(0, 173);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(864, 355);
+            this.panel1.TabIndex = 2;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.comboBox1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30"});
+            this.comboBox1.Location = new System.Drawing.Point(706, 93);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(135, 27);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.Text = "Длинна пароля";
+            // 
+            // checkNonLetter
+            // 
+            this.checkNonLetter.AutoSize = true;
+            this.checkNonLetter.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkNonLetter.Location = new System.Drawing.Point(25, 127);
+            this.checkNonLetter.Name = "checkNonLetter";
+            this.checkNonLetter.Size = new System.Drawing.Size(258, 61);
+            this.checkNonLetter.TabIndex = 4;
+            this.checkNonLetter.Text = "исключить небуквенные символы\r\n ({ } [ ] ( ) / \\ \'\r\n\" ` ~ , ; : . < >).";
+            this.checkNonLetter.UseVisualStyleBackColor = true;
+            // 
+            // checSimilar
+            // 
+            this.checSimilar.AutoSize = true;
+            this.checSimilar.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checSimilar.Location = new System.Drawing.Point(25, 60);
+            this.checSimilar.Name = "checSimilar";
+            this.checSimilar.Size = new System.Drawing.Size(206, 42);
+            this.checSimilar.TabIndex = 3;
+            this.checSimilar.Text = "исключить похожие\r\nсимволы (i, l, 1, L, o, 0, O),\r\n";
+            this.checSimilar.UseVisualStyleBackColor = true;
+            // 
+            // passBox
+            // 
+            this.passBox.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.passBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(98)))), ((int)(((byte)(97)))));
+            this.passBox.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.passBox.Location = new System.Drawing.Point(301, 93);
+            this.passBox.Multiline = true;
+            this.passBox.Name = "passBox";
+            this.passBox.ReadOnly = true;
+            this.passBox.Size = new System.Drawing.Size(354, 37);
+            this.passBox.TabIndex = 1;
+            this.passBox.Text = "Тут будет ваш пароль";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(181)))), ((int)(((byte)(122)))));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(333, 249);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 41);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Сгенерировать";
+            this.button1.UseMnemonic = false;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button1_MouseDown);
+            this.button1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button1_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(72)))), ((int)(((byte)(74)))));
+            this.ClientSize = new System.Drawing.Size(863, 528);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Exit_Button);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Exit_Button;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox passBox;
+        private System.Windows.Forms.CheckBox checkNonLetter;
+        private System.Windows.Forms.CheckBox checSimilar;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PassGeneration
+{
+    public partial class Form1 : Form
+    {
+        private int minLenght = 8;
+        private int maxLength = 30;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Point point;
+        public static string GeneratePassword(int length, bool includeSimilarChars, bool includeNonAlphaNumericChars)
+        {
+            Random random = new Random();
+            const string chars = "ABCDEFGHJKLMNPRSTUVWXYZabcdefghijkmnpqrstuvwxyz0123456789!@#$%^&*_-+=?";
+            const string similarChars = "il1Lo0O";
+            const string nonAlphaNumericChars = "{}[]()/\\'\"`~,;:.<>!@#$%&";
+            string usableChars = chars;
+
+            if (!includeSimilarChars)
+            {
+                foreach (char similarChar in similarChars)
+                {
+                    usableChars = usableChars.Replace(similarChar.ToString(), string.Empty);
+                }
+            }
+
+            if (!includeNonAlphaNumericChars)
+            {
+                usableChars += nonAlphaNumericChars;
+            }
+
+            StringBuilder password = new StringBuilder();
+
+            for (int i = 0; i < length; i++)
+            {
+                password.Append(usableChars[random.Next(usableChars.Length)]);
+            }
+
+
+            string filePath = "rockyou.txt";
+            if (File.Exists(filePath))
+            {
+                foreach (string line in File.ReadLines(filePath))
+                {
+                    if (string.Equals(line, password))
+                    {
+                        return "0";
+                    }
+                }
+            }
+            return password.ToString();
+        }
+
+        private void Exit_Button_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Exit_Button_MouseMove(object sender, MouseEventArgs e)
+        {
+            Exit_Button.ForeColor = Color.Red;
+        }
+
+        private void Exit_Button_MouseLeave(object sender, EventArgs e)
+        {
+            Exit_Button.ForeColor = Color.Black;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int lenght = 0;
+            string password = "0";
+            if (comboBox1.SelectedItem != null)
+            {
+                 lenght = int.Parse(comboBox1.SelectedItem.ToString());
+            }
+            else
+            {
+                password = "0"; 
+            }
+            
+            password = GeneratePassword(lenght, checSimilar.Checked, checkNonLetter.Checked);
+            passBox.Text = password;
+            passBox.ForeColor = Color.Black;
+            if (password == "0")
+            {
+                MessageBox.Show("Ошибка генерации пароля \nПопробуйте снова");
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - point.X;
+                this.Top += e.Y - point.Y;
+            }
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            point = new Point(e.X, e.Y);
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - point.X;
+                this.Top += e.Y - point.Y;
+            }
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            point = new Point(e.X, e.Y);
+        }
+
+        private void textBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - point.X;
+                this.Top += e.Y - point.Y;
+            }
+        }
+
+        private void textBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            point = new Point(e.X, e.Y);
+        }
+
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - point.X;
+                this.Top += e.Y - point.Y;
+            }
+        }
+
+        private void button1_MouseDown(object sender, MouseEventArgs e)
+        {
+            point = new Point(e.X, e.Y);
+        }
+
+    }
+}
