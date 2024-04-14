57f5d6ea9a5c2a5025e0abea2d7c4b5d7d01f369
Karrics <143880497+Karrics@users.noreply.github.com>
2024-04-14 16:52:37 +0300

    Add files via upload

---
+++ TestPassGeneration/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestPassGeneration")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestPassGeneration")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bd87e9de-2a2e-4055-a8ef-5f9325de5441")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestPassGeneration/UnitTest1.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PassGeneration;
+using System;
+
+namespace TestPassGeneration
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GeneratePassword_ReturnsPasswordWithCorrectLength()
+        {
+
+            int length = 10;
+            bool includeSimilarChars = true;
+            bool includeNonAlphaNumericChars = true;
+
+
+            string password = Form1.GeneratePassword(length, includeSimilarChars, includeNonAlphaNumericChars);
+
+
+            Assert.AreEqual(length, password.Length);
+        }
+
+        [TestMethod]
+        public void GeneratePassword_DoesNotContain_SimilarChars()
+        {
+            int length = 12;
+            bool includeSimilarChars = false;
+            bool includeNonAlphaNumericChars = true;
+
+            string password = Form1.GeneratePassword(length, includeSimilarChars, includeNonAlphaNumericChars);
+
+            Assert.IsFalse(password.Contains("0"));
+            Assert.IsFalse(password.Contains("i"));
+            Assert.IsFalse(password.Contains("l"));
+            Assert.IsFalse(password.Contains("1"));
+            Assert.IsFalse(password.Contains("L"));
+            Assert.IsFalse(password.Contains("o"));
+            Assert.IsFalse(password.Contains("O"));
+        }
+
+        [TestMethod]
+        public void GeneratePassword_DoesNotContain_NonAlphaNumericChars()
+        {
+            int length = 15;
+            bool includeSimilarChars = true;
+            bool includeNonAlphaNumericChars = false;
+
+
+            string password = Form1.GeneratePassword(length, includeSimilarChars, includeNonAlphaNumericChars);
+
+
+
+            Assert.IsFalse(password.Contains(")"));
+            Assert.IsFalse(password.Contains("("));
+            Assert.IsFalse(password.Contains("{"));
+            Assert.IsFalse(password.Contains("}"));
+            Assert.IsFalse(password.Contains("["));
+            Assert.IsFalse(password.Contains("]"));
+            Assert.IsFalse(password.Contains("|"));
+            Assert.IsFalse(password.Contains("/"));
+            Assert.IsFalse(password.Contains(","));
+            Assert.IsFalse(password.Contains("."));
+            Assert.IsFalse(password.Contains("<"));
+            Assert.IsFalse(password.Contains(">"));
+            Assert.IsFalse(password.Contains("!"));
+            Assert.IsFalse(password.Contains("@"));
+            Assert.IsFalse(password.Contains("#"));
+            Assert.IsFalse(password.Contains("$"));
+            Assert.IsFalse(password.Contains("%"));
+            Assert.IsFalse(password.Contains("&"));
+        }
+
+        [TestMethod]
+        public void GeneratePassword_DoesNotContain()
+        {
+            int length = 15;
+            bool includeSimilarChars = false;
+            bool includeNonAlphaNumericChars = false;
+
+
+            string password = Form1.GeneratePassword(length, includeSimilarChars, includeNonAlphaNumericChars);
+
+
+
+            Assert.IsFalse(password.Contains(")"));
+            Assert.IsFalse(password.Contains("("));
+            Assert.IsFalse(password.Contains("{"));
+            Assert.IsFalse(password.Contains("}"));
+            Assert.IsFalse(password.Contains("["));
+            Assert.IsFalse(password.Contains("]"));
+            Assert.IsFalse(password.Contains("|"));
+            Assert.IsFalse(password.Contains("/"));
+            Assert.IsFalse(password.Contains(","));
+            Assert.IsFalse(password.Contains("."));
+            Assert.IsFalse(password.Contains("<"));
+            Assert.IsFalse(password.Contains(">"));
+            Assert.IsFalse(password.Contains("!"));
+            Assert.IsFalse(password.Contains("@"));
+            Assert.IsFalse(password.Contains("#"));
+            Assert.IsFalse(password.Contains("$"));
+            Assert.IsFalse(password.Contains("%"));
+            Assert.IsFalse(password.Contains("&"));
+            Assert.IsFalse(password.Contains("0"));
+            Assert.IsFalse(password.Contains("i"));
+            Assert.IsFalse(password.Contains("l"));
+            Assert.IsFalse(password.Contains("1"));
+            Assert.IsFalse(password.Contains("L"));
+            Assert.IsFalse(password.Contains("o"));
+            Assert.IsFalse(password.Contains("O"));
+        }
+
+        [TestMethod]
+        public void GeneratePassword_ReturnsPasswordWithCorrectLength2()
+        {
+
+            int length = 0;
+            bool includeSimilarChars = true;
+            bool includeNonAlphaNumericChars = true;
+
+
+            string password = Form1.GeneratePassword(length, includeSimilarChars, includeNonAlphaNumericChars);
+
+
+            Assert.AreEqual(length, password.Length);
+        }
+    }
+}
+
